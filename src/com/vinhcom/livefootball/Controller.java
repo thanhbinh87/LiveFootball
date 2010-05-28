6f700cae4c14317307dc4f209342555f090307da
AloneRoad <thokct@gmail.com>
2010-05-29 02:32:44 +0700

    first commit

--- src/com/vinhcom/livefootball/Controller.java
+++ src/com/vinhcom/livefootball/Controller.java
@@ -12,73 +12,117 @@ import java.util.Vector;
 import com.vinhcom.livefootball.Utilities.*;
 
 public class Controller extends MIDlet implements ActionListener {
-    public String root_url = "http://localhost:4001/menu";
-    public Vector href_list = new Vector();
-    private Form form;
-    private Command mBackCommand = new Command("Quay lại");
-    private Command selectCommand = new Command("Chọn");
-    private Command exitCommand = new Command("Thoát");
+  private String SELECT = "select";
+  private String BACK = "back";
+  private String EXIT = "exit";
+  private String LIST = "list";
+  private String TREE = "tree";
+  private String HREF_TEMPLATE = "";
+  private String TYPE_TEMPLATE = "";
+  private String NAME_TEMPLATE = "";
+  
 
-    private void list_display(String json_string) {
-        String form_title = Utilities.get_text(json_string, "\"form_title\": \"", "\"");
-        form = new Form(form_title);
-        form.setLayout(new BorderLayout());
-        List list = new List();
-        String items_string = Utilities.get_text(json_string, "\"items\": [", "]");
-        String items[];
-        items = Utilities.split(items_string, "},");
-        for (int i = 0; i < items.length; i++) {
-            String item = Utilities.get_text(items[i], "\"name\": \"", "\"");
-            list.addItem(item);
-            String href = root_url + "/"
-                        + Utilities.get_text(items[i], "\"href\": \"", "\"");
-            System.out.println(href);
-            href_list.addElement(href);
-        }
-        form.addComponent(BorderLayout.CENTER, list);
-        form.show();
-        
-        form.addCommand(selectCommand);
-        form.addCommand(exitCommand);
-        form.setCommandListener(this);
+  private String root_url = "http://localhost:4001";
+  private Command back_command = new Command("Quay lại");
+  private Command select_command = new Command("Chọn");
+  private Command exit_command = new Command("Thoát");
+  private Vector href_list = new Vector();
+  private Form form;
+  private List list;
+
+  /*
+   *
+   * Điều khiển việc hiển thị list bất kỳ
+   *
+   */
+  private void list_display(String json_string) {
+    String form_title = Utilities.get_text(json_string, "\"form_title\": \"", "\"");
+    form = new Form(form_title);  // tạo form với tên form lấy được trong chuỗi trả về
+    list = new List();
+    form.setLayout(new BorderLayout());
+    String items_string = Utilities.get_text(json_string, "\"items\": [", "]");
+    String items[];
+    items = Utilities.split(items_string, "},");
+    for (int i = 0; i < items.length; i++) {
+      String item = Utilities.get_text(items[i], "\"name\": \"", "\"");
+      list.addItem(item);
+      String href = root_url + "/" + Utilities.get_text(items[i], "\"href\": \"", "\"");
+      href_list.addElement(href);
+    }
+    form.addComponent(BorderLayout.CENTER, list);
+    form.show();
+
+    // Phân tích chuỗi trả về, lấy ra quy định bố trí nút bấm
+    String left_button = Utilities.get_text(json_string, "\"left_button\": \"", "\"");
+    String right_button = Utilities.get_text(json_string, "\"right_button\": \"", "\"");
+
+    if (left_button.equals(SELECT)) {
+      form.addCommand(select_command);
+    } else if (left_button.equals(EXIT)) {
+      form.addCommand(exit_command);
+    } else if (left_button.equals(BACK)) {
+      form.addCommand(back_command);
     }
 
-    private void tree_display(String json_string) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    if (right_button.equals(SELECT)) {
+      form.addCommand(select_command);
+    } else if (right_button.equals(EXIT)) {
+      form.addCommand(exit_command);
+    } else if (right_button.equals(BACK)) {
+      form.addCommand(back_command);
+    }
+    form.setCommandListener(this); // Chờ đến khi có một nút được bấm
     }
 
-    public void actionPerformed(ActionEvent ae) {
-//      System.out.println(ae.getCommand());
-      if (ae.getCommand() == selectCommand) {
-        System.out.println("Vừa bấm chọn");
-//        form.show();
+  private void tree_display(String json_string) {
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
+
+  /*
+   * Điều khiển việc thực hiện và chuyển hướng các lệnh (nút trái và phải)
+   *
+   */
+  public void actionPerformed(ActionEvent ae) {
+    if (ae.getCommand() == exit_command) {
+      // thoát
+      } else if (ae.getCommand() == back_command) {
+      // quay trở lại
       }
-    }
 
-    public void startApp() {
-      if (form == null) {
-        Display.init(this);
-        try {
-            Resources r = Resources.open("/com/vinhcom/livefootball/default.res");
-            UIManager.getInstance().setThemeProps(r.getTheme("default"));
-        } catch (IOException ioe) {
-            System.out.println("Couldn't load theme.");
-        }
-        String json_string = Utilities.urlopen(root_url);
-        String type = Utilities.get_text(json_string, "\"type\": \"", "\"");
-        if (type.equals("list")) {
-            list_display(json_string);
-        } else if (type.equals("tree")) {
-            tree_display(json_string);
-        }
-      }
-      else {
-      form.show();
-      }
+    if (ae.getCommand() == select_command) { // nếu bấm nút "Chọn"
+      int index = list.getSelectedIndex();  // lấy id của đối tượng được chọn (id bắt đầu từ 0)
+      String url = (String) href_list.elementAt(index); // lấy url tương ứng với đối tượng được chọn
+      System.out.println(url);
+      display(url);
     }
-    public void pauseApp() {
-    }
+  }
 
-    public void destroyApp(boolean unconditional) {
+  private void display(String url) {
+    String json_string = Utilities.urlopen(url);
+    String type = Utilities.get_text(json_string, "\"type\": \"", "\"");
+    
+    if (type.equals(LIST)) {
+      list_display(json_string);
+    } else if (type.equals(TREE)) {
+      tree_display(json_string);
     }
+  }
+
+  public void startApp() {
+      Display.init(this);
+      try {
+        Resources r = Resources.open("/com/vinhcom/livefootball/default.res");
+        UIManager.getInstance().setThemeProps(r.getTheme("default"));
+      } catch (IOException ioe) {
+        System.out.println("Couldn't load theme.");
+      }
+      String url = root_url + "/menu";
+      display(url);
+  }
+
+  public void pauseApp() {
+  }
+
+  public void destroyApp(boolean unconditional) {
+  }
 }
--- src/com/vinhcom/livefootball/Utilities.java
+++ src/com/vinhcom/livefootball/Utilities.java
@@ -1,245 +1,102 @@
 package com.vinhcom.livefootball;
 
-
-
-
-
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Vector;
 import javax.microedition.io.Connector;
 import javax.microedition.io.HttpConnection;
-import javax.microedition.lcdui.Display;
-import javax.microedition.lcdui.List;
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author win7_64
- */
 
 public class Utilities {
-
-    private static String root_url = "http://203.128.246.60:8000/";
-    private static String matches_url = "http://203.128.246.60:8000/matches";
-    private static String comments_url = "http://203.128.246.60:8000/comments";
-    // Main MIDP display
-    private Display myDisplay = null;
-    // GUI component for submitting request
-    private List list;
-    private String[] menuItems;
-    // GUI for matches
-    private List matches_list;
-    private String[] matches_menu;
-    private List comments_list;
-    private String[] comments_menu;
-    private List odds_list;
-    private String[] odds_menu;
-    Vector bet_info_href = new Vector();
-
-    private String[] get_matches() {
-        String result;
-        result = urlopen(matches_url);
-        Vector menu = new Vector();
-        String[] items = split(result, "/match>");
-
-        for (int i = 0; i < items.length; i++) {
-            String item = get_text(items[i], ">", "<");
-            String href_link = get_text(items[i], "href2=\"", "\" ");
-            if (item != null) {
-                menu.addElement(item.trim());
-                bet_info_href.addElement(root_url + href_link.trim());
-            }
-        }
-
-        String[] _menu = new String[menu.size()];
-        if (menu.size() > 0) {
-            for (int i = 0; i < menu.size(); i++) {
-                _menu[i] = (String) menu.elementAt(i);
-            }
-        }
-        return _menu;
+  public static String replace(String text, String searchString, String replacementString) {
+    StringBuffer sBuffer = new StringBuffer();
+    int pos = 0;
+    while ((pos = text.indexOf(searchString)) != -1) {
+      sBuffer.append(text.substring(0, pos) + replacementString);
+      text = text.substring(pos + searchString.length());
     }
+    sBuffer.append(text);
+    return sBuffer.toString();
+  }
 
-    private String[] bet_info(int match_index) {
-        String result;
-        Vector odd_info = new Vector();
-        String url = (String) bet_info_href.elementAt(match_index);
-        url = replace(url, " ", "%20");
-        url = replace(url, "|", "%7C");
-        result = urlopen(url);
-        String match = get_text(result, "<match>", "</match>");
-        String source = get_text(result, "<source>", "</source>");
-        String date = get_text(result, "<date>", "</date>");
-        String hour = get_text(result, "<hour>", "</hour>");
-        String team_1 = get_text(result, "<team_1>", "</team_1>");
-        String team_2 = get_text(result, "<team_2>", "</team_2>");
-        String ah = get_text(result, "<ah>", "</ah>");
-        String ah_team_1 = get_text(result, "<ah_team_1>", "</ah_team_1>");
-        String ah_team_2 = get_text(result, "<ah_team_2>", "</ah_team_2>");
-        String ou = get_text(result, "<ou>", "</ou>");
-        String ou_team_1 = get_text(result, "<ou_team_1>", "</ou_team_1>");
-        String ou_team_2 = get_text(result, "<ou_team_2>", "</ou_team_2>");
-        String ah1st = get_text(result, "<ah1st>", "</ah1st>");
-        String ah1st_team_1 = get_text(result, "<ah1st_team_1>", "</ah1st_team_1>");
-        String ah1st_team_2 = get_text(result, "<ah1st_team_2>", "</ah1st_team_2>");
-        String ou1st = get_text(result, "<ou1st>", "</ou1st>");
-        String ou1st_team_1 = get_text(result, "<ou1st_team_1>", "</ou1st_team_1>");
-        String ou1st_team_2 = get_text(result, "<ou1st_team_2>", "</ou1st_team_2>");
+  public static String get_text(String str, String tag) {
+    return get_text(str, tag, tag);
+  }
 
-        odd_info.addElement("Nguồn: " + source);
-        odd_info.addElement("Ngày: " + date);
-        odd_info.addElement("Giờ: " + hour);
-        odd_info.addElement("Trận đấu: " + match);
-        odd_info.addElement("Toàn trận");
-        odd_info.addElement(" Chấp");
-        odd_info.addElement("  " + team_1 + ": +" + ah + "@" + ah_team_1);
-        odd_info.addElement("  " + team_2 + ": -" + ah + "@" + ah_team_2);
-        odd_info.addElement(" Trên dưới");
-        odd_info.addElement("  " + team_1 + ": +" + ou + "@" + ou_team_1);
-        odd_info.addElement("  " + team_2 + ": -" + ou + "@" + ou_team_2);
-        odd_info.addElement("Hiệp 1");
-        odd_info.addElement(" Chấp");
-        odd_info.addElement("  " + team_1 + ": +" + ah1st + "@" + ah1st_team_1);
-        odd_info.addElement("  " + team_2 + ": -" + ah1st + "@" + ah1st_team_2);
-        odd_info.addElement(" Trên dưới");
-        odd_info.addElement("  " + team_1 + ": +" + ou1st + "@" + ou1st_team_1);
-        odd_info.addElement("  " + team_2 + ": -" + ou1st + "@" + ou1st_team_2);
-
-
-        String[] _menu = new String[odd_info.size()];
-        if (odd_info.size() > 0) {
-            for (int i = 0; i < odd_info.size(); i++) {
-                _menu[i] = (String) odd_info.elementAt(i);
-//                System.out.println(_menu[i]);
-            }
-        }
-        return _menu;
-
+  public static String get_text(String str, String open, String close) {
+    if (str == null || open == null || close == null) {
+      return null;
     }
-
-    private String[] get_comments() {
-        String result;
-        result = urlopen(comments_url);
-        Vector menu = new Vector();
-        String[] items = split(result, "/comment>");
-
-        for (int i = 0; i < items.length; i++) {
-            String item = get_text(items[i], ">", "<");
-            if (item != null) {
-                menu.addElement(item.trim());
-                System.out.println(item);
-            }
-        }
-
-        String[] _menu = new String[menu.size()];
-        if (menu.size() > 0) {
-            for (int i = 0; i < menu.size(); i++) {
-                _menu[i] = (String) menu.elementAt(i);
-            }
-        }
-        return _menu;
-
+    int start = str.indexOf(open);
+    if (start != -1) {
+      int end = str.indexOf(close, start + open.length());
+      if (end != -1) {
+        return str.substring(start + open.length(), end);
+      }
     }
+    return null;
+  }
 
-    public static String replace(String text,
-            String searchString,
-            String replacementString) {
-        StringBuffer sBuffer = new StringBuffer();
-        int pos = 0;
-        while ((pos = text.indexOf(searchString)) != -1) {
-            sBuffer.append(text.substring(0, pos) + replacementString);
-            text = text.substring(pos + searchString.length());
-        }
-        sBuffer.append(text);
-        return sBuffer.toString();
+  public static boolean is_empty(String str) {
+    return str == null || str.length() == 0;
+  }
+
+  public static String[] split(String original, String sep) {
+    Vector nodes = new Vector();
+    String separator = sep;
+    int index = original.indexOf(separator);
+    while (index >= 0) {
+      nodes.addElement(original.substring(0, index));
+      original = original.substring(index + separator.length());
+      index = original.indexOf(separator);
     }
+    nodes.addElement(original);
 
-    public static String get_text(String str, String tag) {
-        return get_text(str, tag, tag);
+    String[] result = new String[nodes.size()];
+    if (nodes.size() > 0) {
+      for (int loop = 0; loop < nodes.size(); loop++) {
+        result[loop] = (String) nodes.elementAt(loop);
+      }
     }
+    return result;
+  }
 
-    public static String get_text(String str,
-            String open,
-            String close) {
-        if (str == null || open == null || close == null) {
-            return null;
+  public static String urlopen(String url) {
+    HttpConnection cn = null;
+    InputStream str = null;
+
+    StringBuffer sb = null;
+    try {
+      int code;
+      cn = (HttpConnection) Connector.open(url);
+      code = cn.getResponseCode();
+      if (code == HttpConnection.HTTP_OK) {
+        str = cn.openInputStream();
+        sb = new StringBuffer();
+
+        InputStreamReader r = new InputStreamReader(str, "UTF-8");
+        int total = 0;
+        int read = 0;
+
+        while ((read = r.read()) >= 0) {
+          total++;
+          sb.append((char) read);
         }
-        int start = str.indexOf(open);
-        if (start != -1) {
-            int end = str.indexOf(close, start + open.length());
-            if (end != -1) {
-                return str.substring(start + open.length(), end);
-            }
+        return sb.toString();
+      } else {
+        throw new Exception("Gặp lỗi trong quá trình kết nối");
+      }
+    } catch (Exception e) {
+      return null;
+    } finally {
+      try {
+        if (str != null) {
+          str.close();
         }
-        return null;
+        if (cn != null) {
+          cn.close();
+        }
+      } catch (Exception e) {
+      }
     }
-
-    public static boolean is_empty(String str) {
-        return str == null || str.length() == 0;
-    }
-
-    public static String[] split(String original, String sep) {
-        Vector nodes = new Vector();
-        String separator = sep;
-        int index = original.indexOf(separator);
-        while (index >= 0) {
-            nodes.addElement(original.substring(0, index));
-            original = original.substring(index + separator.length());
-            index = original.indexOf(separator);
-        }
-        nodes.addElement(original);
-
-        String[] result = new String[nodes.size()];
-        if (nodes.size() > 0) {
-            for (int loop = 0; loop < nodes.size(); loop++) {
-                result[loop] = (String) nodes.elementAt(loop);
-            }
-        }
-        return result;
-    }
-
-    public static String urlopen(String url) {
-        HttpConnection cn = null;
-        InputStream str = null;
-        //DataInputStream strd = null;
-
-        StringBuffer sb = null;
-        try {
-            int code;
-            cn = (HttpConnection) Connector.open(url);
-            code = cn.getResponseCode();
-            if (code == HttpConnection.HTTP_OK) {
-                str = cn.openInputStream();
-                sb = new StringBuffer();
-
-                InputStreamReader r = new InputStreamReader(str, "UTF-8");
-                int total = 0;
-                int read = 0;
-
-                while ((read = r.read()) >= 0) {
-                    total++;
-                    sb.append((char) read);
-                }
-                return sb.toString();
-            } else {
-                throw new Exception("Gặp lỗi trong quá trình kết nối");
-            }
-        } catch (Exception e) {
-            return null;
-        } finally {
-            try {
-                if (str != null) {
-                    str.close();
-                }
-                if (cn != null) {
-                    cn.close();
-                }
-            } catch (Exception e) {
-            }
-        }
-    }//end urlopen(String)
+  }//end urlopen(String)
 }
